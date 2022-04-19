061602c288ef72c074da571e483382dc35abb5f4
Alan Chan <alanslc78@hotmail.com>
2022-04-19 19:17:49 +0800

    Add operate() and 4 algorithm function (empty)

---
+++ scripts/script.js
@@ -0,0 +1,37 @@
+function operate(a, b, op) {
+   let result;
+
+   switch (op) {
+      case '+':
+         result = add(a, b);
+         break;
+      case '-':
+         result = sub(a, b);
+         break;
+      case '*':
+         result = mul(a, b);
+         break;
+      case '/':
+         result = div(a, b);
+         break;
+      default:
+   }
+
+   return result;
+}
+
+function add(a, b) {
+
+}
+
+function sub(a, b) {
+
+}
+
+function mul(a, b) {
+
+}
+
+function div(a, b) {
+
+}
