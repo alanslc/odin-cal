150d9da2e100314828aa3d5e86b3ec963f7883e2
Alan Chan <alanslc78@hotmail.com>
2022-04-20 12:33:38 +0800

    Modify numpad can input and display up to 16 digit

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,64 @@
-const MAX_DIGIT = 16;
-
 const cal = new Calculator();
 
 function Calculator() {
+   const MAX_DIGIT = 16;
+   const INPUT_STATUE_NONE = 0;
+   const INPUT_STATUE_LEFT_OPERAND = 1;
+   const INPUT_STATUE_RIGHT_OPERAND = 2;
+
    this.leftOperand = null;
    this.rightOperand = null;
    this.op = null;
    this.displayNumber = 0;
+   this.inputState = INPUT_STATUE_NONE;
 
-   this.init = function() {
+   this.init = function () {
+      const pad = document.querySelector('.pad');
+      pad.addEventListener('click', this.keyPressedByMouse.bind(this));
 
+      this.leftOperand = null;
+      this.rightOperand = null;
+      this.op = null;
+      this.displayNumber = new Num();
+      this.inputState = INPUT_STATUE_NONE;
+      this.display();
    };
-   
-   this.display = function() {
+
+   this.display = function () {
       const disp = document.querySelector('.disp');
-      disp.innerText = this.displayNumber;
+
+      let n;
+      switch (this.inputState) {
+         case INPUT_STATUE_LEFT_OPERAND:
+            n = this.leftOperand; break;
+         case INPUT_STATUE_RIGHT_OPERAND:
+            n = this.rightOperand; break;
+         default:
+            n = new Num();
+      }
+      disp.innerText = n.toString();
+      console.log('display')
    };
 
-   this.display();
+   this.keyPressedByMouse = function (e) {
+      const btn = e.target;
+      if (btn.classList.contains('num')) {
+         if (this.inputState == INPUT_STATUE_NONE) {
+            this.leftOperand = new Num();
+            this.inputState = INPUT_STATUE_LEFT_OPERAND;
+         }
 
-   this.keyPressed = function() {
-
+         if (this.inputState == INPUT_STATUE_LEFT_OPERAND) {
+            if (this.leftOperand.len() < MAX_DIGIT) {
+               this.leftOperand.inputDigit(btn.dataset.key);
+               this.display();
+            }
+         }
+         // this.displayNumber = buttonPressed.dataset.key;
+      }
    };
+
+   this.init();
 }
 
 function Num(intPart = '', fraction = '', sign = '') {
@@ -39,7 +76,15 @@ function Num(intPart = '', fraction = '', sign = '') {
    }
 
    this.inputDigit = function (ch) {
-      return this;
+      if (ch == null || ch == undefined || isNaN(ch))
+         return;
+
+      if (ch >= 0 && ch <= 9) {
+         if (this.inputIntPart)
+            this.intPart += ch;
+         else
+            this.fraction += ch;
+      }
    }
 
    this.removeDigit = function () {
@@ -48,6 +93,13 @@ function Num(intPart = '', fraction = '', sign = '') {
 
    this.toggleSign = function () {
       this.sign = this.sign ? '-' : '';
+   }
+
+   this.toString = function () {
+      if (this.intPart == '')
+         return '0';
+      else
+         return this.intPart;
    }
 }
 
