12f6c568ad250e12190133a6a513ed5e5fa64963
Alan Chan <alanslc78@hotmail.com>
2022-04-20 11:26:58 +0800

    Add Calculator object

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,56 @@
+const MAX_DIGIT = 16;
+
+const cal = new Calculator();
+
+function Calculator() {
+   this.leftOperand = null;
+   this.rightOperand = null;
+   this.op = null;
+   this.displayNumber = 0;
+
+   this.init = function() {
+
+   };
+   
+   this.display = function() {
+      const disp = document.querySelector('.disp');
+      disp.innerText = this.displayNumber;
+   };
+
+   this.display();
+
+   this.keyPressed = function() {
+
+   };
+}
+
+function Num(intPart = '', fraction = '', sign = '') {
+   this.inputIntPart = true;
+   this.sign = '';
+   this.intPart = '';
+   this.fraction = ''
+
+   this.len = function () {
+      return this.intPart.length + this.fraction.length;
+   };
+
+   this.value = function () {
+      return 0;
+   }
+
+   this.inputDigit = function (ch) {
+      return this;
+   }
+
+   this.removeDigit = function () {
+      return this;
+   }
+
+   this.toggleSign = function () {
+      this.sign = this.sign ? '-' : '';
+   }
+}
+
 function operate(a, b, op) {
    let result;
 
